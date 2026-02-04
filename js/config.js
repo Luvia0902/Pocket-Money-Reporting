292b1c563ac74f1fd728e2df75c6361ab7f25b66
MoneyApp User <user@local.app>
2026-02-04 16:23:31 +0800

    Security: Move firebase config to gitignore and add example file

--- js/config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyAk9KWKqjtxkCK6tAu077lO-AaHmDir83M",
-    authDomain: "zeromoney-db.firebaseapp.com",
-    projectId: "zeromoney-db",
-    storageBucket: "zeromoney-db.firebasestorage.app",
-    messagingSenderId: "643017041305",
-    appId: "1:643017041305:web:c05a3ed1b492520c0fdf08",
-    measurementId: "G-DH4V0Z1C73"
-};
